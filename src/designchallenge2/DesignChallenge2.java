55bc938d4434757fd828eb3b13c07ab6c2850a2a
Luis Lopez <luis_lopez@dlsu.edu.ph>
2018-03-13 21:13:30 +0800

    Removed commented line from driver

--- src/designchallenge2/DesignChallenge2.java
+++ src/designchallenge2/DesignChallenge2.java
@@ -10,7 +10,6 @@ import designchallenge2.view.DefaultCalendarView;
 
 public class DesignChallenge2 {
 	public static void main(String[] args) {
-		//new DefaultCalendarController(new DefaultCalendarView()).init();
 		CalendarModel model = new DefaultCalendarModel(new CalendarDB());
 		CalendarController controller = new DefaultCalendarController(model);
 		CalendarObserver view = new DefaultCalendarView(model, controller);
